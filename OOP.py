9e4b1296e0cd8f094f14ffa7d6351410350ef8d9
Martin <martin.harnik91@gmail.com>
2024-12-30 20:41:31 +0100

    Lesson 03: Class methods and static methods

--- OOP.py
+++ OOP.py
@@ -18,25 +18,35 @@ class Employee:
         self.pay = int(self.pay * self.raise_amount)
         #self.pay = int(self.pay * Employee.raise_amount) this works too, but raise amount wont be changed for individual instances (say 1.08 for emp1 and 1.11 for emp2) 
 
-print(f"Now the total number of employees is {Employee.number_of_employees}.")
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    # Additional way of constructing an instance
+    @classmethod
+    def from_string(cls, employee_string):
+        first, last, pay = employee_string.split("-")
+        return cls(first, last, pay)
+    
+    # This is a static method
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        else:
+            return True
+
+new_emp = Employee.from_string("Mark-Brown-18000")
+print(new_emp.fullname())
 
 emp = Employee("John", "Green", 15000)
 
-print(Employee.raise_amount)
-print(emp.raise_amount)
-
-Employee.raise_amount = 1.08
+Employee.set_raise_amount(1.06)
 
 print(Employee.raise_amount)
 print(emp.raise_amount)
 
-emp.raise_amount = 1.11
-
-print(Employee.raise_amount)
-print(emp.raise_amount)
-
-print(emp.pay)
-emp.apply_raise()
-print(emp.pay)
-
-print(f"But now after welcoming {emp.fullname()} to the company the total number of employees is {Employee.number_of_employees}.")
+# How static method works
+import datetime
+my_date = datetime.date(2024, 8, 23)
+print(Employee.is_workday(my_date))
