906fb61e42a9e611c2d6d8d155908b3ee5425a82
Martin <martin.harnik91@gmail.com>
2025-01-01 11:45:18 +0100

    Lesson 04: Inheritance

--- OOP.py
+++ OOP.py
@@ -12,7 +12,7 @@ class Employee:
         Employee.number_of_employees += 1
 
     def fullname(self):
-        return '{} {}'.format(self.first, self.last)
+        return f'{self.first} {self.last}'
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
@@ -36,17 +36,58 @@ class Employee:
         else:
             return True
 
-new_emp = Employee.from_string("Mark-Brown-18000")
-print(new_emp.fullname())
+class Developer(Employee):
+
+    raise_amount = 1.15
+
+    def __init__(self, first, last, pay, programming_language):
+        super().__init__(first, last, pay)
+        #Employee.__init__(self, first, last, pay) This will also work fine.
+        self.programming_language = programming_language
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees = None):
+        super().__init__(first, last, pay)
+
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_employee(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_employee(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def show_employees(self):
+        for emp in self.employees:
+            print(emp.fullname())         
 
 emp = Employee("John", "Green", 15000)
+dev = Developer("Jeff", "Scott", 20000, "Python")
+mgr = Manager("Dirk", "Gibbson", 50000)
 
-Employee.set_raise_amount(1.06)
+# Show the full name of the manager
+print(mgr.fullname())
 
-print(Employee.raise_amount)
-print(emp.raise_amount)
+# Add employees to the manager
+mgr.add_employee(dev)
+mgr.add_employee(emp)
 
-# How static method works
-import datetime
-my_date = datetime.date(2024, 8, 23)
-print(Employee.is_workday(my_date))
+# Show the names of employees who report to the manager
+mgr.show_employees()
+
+# isinstance function
+print(isinstance(mgr, Manager))
+print(isinstance(mgr, Employee))
+print(isinstance(dev, Manager))
+
+# issubclass function
+print(issubclass(Manager, Employee))
+print(issubclass(Developer, Employee))
+print(issubclass(Developer, Manager))
+
+
